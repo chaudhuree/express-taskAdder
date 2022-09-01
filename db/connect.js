da5784e4294201294fa405cf672eced14aa614d7
chaudhuree <chaudhuree00@gmail.com>
2022-09-01 11:12:13 +0600

    docs: make sure db connect first then start the app by creating and using start function

--- db/connect.js
+++ db/connect.js
@@ -2,12 +2,30 @@ const mongoose = require('mongoose')
 
 const connectionString = "mongodb+srv://sohan:1234@expresstaskmanager.grxys5y.mongodb.net/EXPRESS-TASK-MANAGER?retryWrites=true&w=majority"
 
+const connectDB = (url) => {
+  // second parameter for get relieve from console depricative notice
+
+  return mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+}
+
+
+
+module.exports = connectDB
+
+
+
+// for tesing at the first case
 // second parameter for get relieve from console depricative notice
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("CONNECTION ESTABLISHED WITH DB"))
-.catch(err => console.log(err))
+// mongoose.connect(connectionString, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log("CONNECTION ESTABLISHED WITH DB"))
+// .catch(err => console.log(err))
