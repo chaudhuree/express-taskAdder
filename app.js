b8e8f2315825a17f712e3789ebeb6e0c37318f25
chaudhuree <chaudhuree00@gmail.com>
2022-09-13 21:31:37 +0600

    docs: make sure db connect first then start the app by creating and using start function

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// database
-require('./db/connect')
+
 const express = require('express')
 // router
-const tasks =require('./routes/tasks')
+const tasks = require('./routes/tasks')
 
 const app = express()
 const port = 3000
 
+
+// database
+const connectDB = require('./db/connect')
+
+// app dependencies
 app.use(express.json())
 
 // app.get('/api/v1/tasks')          -get all the tasks
@@ -16,8 +20,16 @@ app.use(express.json())
 // app.post('/api/v1/tasks/:id')     -delete task
 
 // router
-app.use('/api/v1/tasks',tasks)
+app.use('/api/v1/tasks', tasks)
 
 // for other routes rather than the original one
-app.all('*',(req, res) =>res.send('you are on wrong way boy'))
-app.listen(port,console.log(` server is running on port no ${port}`))
+app.all('*', (req, res) => res.send('you are on wrong way boy'))
+
+
+const start = async () => {
+  await connectDB()
+app.listen(port, console.log(` server is running on port no ${port}`))
+}
+
+// call  start function
+start()
