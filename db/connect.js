82c7007172b0b36fc9a5300fccf607950c1c6ebf
chaudhuree <chaudhuree00@gmail.com>
2022-09-13 21:17:33 +0600

    docs: for using mongoose 5.X.X version make some changes

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,13 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 
-const connectionString="mongodb+srv://sohan:1234@expresstaskmanager.grxys5y.mongodb.net/EXPRESS-TASK-MANAGER?retryWrites=true&w=majority"
+const connectionString = "mongodb+srv://sohan:1234@expresstaskmanager.grxys5y.mongodb.net/EXPRESS-TASK-MANAGER?retryWrites=true&w=majority"
 
-mongoose.connect(connectionString).then(()=>console.log("CONNECTION ESTABLISHED WITH DB")).catch(err=>console.log(err))
+// second parameter for get relieve from console depricative notice
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("CONNECTION ESTABLISHED WITH DB"))
+.catch(err => console.log(err))
