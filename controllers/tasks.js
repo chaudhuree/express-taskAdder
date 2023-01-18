b1859d288eb0686eb222b0c1e0506b72ebe63159
chaudhuree <chaudhuree00@gmail.com>
2023-01-18 22:52:32 +0600

    1️⃣ import model asyncWrapper and errorFunction

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,18 +1,24 @@
-const getAllTasks=(req, res) =>{
-  res.json({"respnse":'get all tasks'});
-}
-const createTask=(req, res) =>{
-  res.json({"response":'create task'});
-}
-const getTask=(req, res) =>{
-  res.json({"response":'get single task'});
-}
-const updateTask=(req, res) =>{
-  res.json({"response":'update task'});
-}
-const deleteTask=(req, res) =>{
-  res.json({"response":'delete task'});
-}
+const Task=require('../models/Task');
+const asyncWrapper=require('../middleware/async');
+const { createCustomError } = require('../errors/custom-error')
 
 
-module.exports={getAllTasks,createTask,deleteTask,getTask,updateTask}
+module.exports={getAllTasks,createTask,deleteTask,getTask,updateTask,deleteTask}
+
+
+// dummy code
+// const getAllTasks=(req, res) =>{
+//   res.json({"response":'get all tasks'});
+// }
+// const createTask=(req, res) =>{
+//   res.json({"response":'create task'});
+// }
+// const getTask=(req, res) =>{
+//   res.json({"response":'get single task'});
+// }
+// const updateTask=(req, res) =>{
+//   res.json({"response":'update task'});
+// }
+// const deleteTask=(req, res) =>{
+//   res.json({"response":'delete task'});
+// }
