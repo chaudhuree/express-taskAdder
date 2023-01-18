85651d748b1352ee4ae7fa5f81c095d1e2f27ae1
chaudhuree <chaudhuree00@gmail.com>
2023-01-18 22:39:33 +0600

    dynamic port func added & define how to use

---
+++ README.md
@@ -0,0 +1,7 @@
+run on terminal:
+
+```
+  PORT=6000 node app.js
+```
+
+> this will run this app on port 6000
--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 const tasks = require('./routes/tasks')
 
 const app = express()
-const port = 3000
 
 
 // database
@@ -27,6 +26,8 @@ app.use('/api/v1/tasks', tasks)
 // for other routes rather than the original one
 app.use(notFound)
 app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
