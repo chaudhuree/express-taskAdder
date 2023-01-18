e8a38038879ba07fa172bcf33bc2a459c9a4e16d
chaudhuree <chaudhuree00@gmail.com>
2023-01-18 23:16:52 +0600

    5️⃣ docs: delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -36,6 +36,16 @@ const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({})
   res.status(200).json({ tasks })
 })
+
+// docs: delete task 
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOneAndDelete({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+  res.status(200).json({ task })
+})
 module.exports = { getAllTasks, createTask, deleteTask, getTask, updateTask, deleteTask }
 
 
