3fdce6f0bf329874ca826b8a302843ab21f2b342
chaudhuree <chaudhuree00@gmail.com>
2023-01-18 23:03:10 +0600

    2️⃣ 𓃊 create task functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,13 @@ const Task=require('../models/Task');
 const asyncWrapper=require('../middleware/async');
 const { createCustomError } = require('../errors/custom-error')
 
+// create a task
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+})
+
+
 
 module.exports={getAllTasks,createTask,deleteTask,getTask,updateTask,deleteTask}
 
